0423fb4eddbc530aaa4d04b9afd423271adf283f
Kawamura Shingo <36791900+kawamurashingo@users.noreply.github.com>
2022-12-17 19:20:33 +0900

    Update telegram.sh

--- telegram.sh
+++ telegram.sh
@@ -11,19 +11,23 @@ DIR=`dirname $0`
 
 cd ${DIR}
 
+test -d ./FILE || mkdir ./FILE
+test -d ./DSC || mkdir ./DSC
+
+FILE_DATE=`date +%Y%m%d%H%M`
+OLD_FILE_DATE=`ls -ltr ./FILE/ |tail -n2|head -n1 |awk '{print $9}'`
+
 # get google calender schedule
 python3 get_events.py | sed -e 's:<html-blob>::g' -e 's:</html-blob>::g' -e "s:<br>:\n:g" > schedule.txt
 
 # make text
-sed -e "s/DATE/`date +%Y-%m-%d --date tomorrow`/" reverse.sed > make_reverse.sed
-sed -n "/`date +%Y-%m-%d --date tomorrow`/,/^$/p" schedule.txt  | sed -f make_reverse.sed | sed -e "s:`date +%Y-%m-%d --date tomorrow`:`date +%m/%d --date tomorrow`:" -e "s/~.*//" > make.txt
+sed -e "s/DATE/`date +%Y-%m-%d`/" reverse.sed > make_reverse.sed
+sed -n "/`date +%Y-%m-%d`/,/^$/p" schedule.txt  | sed -f make_reverse.sed | sed -e "s:`date +%Y-%m-%d`:`date +%m/%d`:" -e "s/~.*//" > make.txt
 
-cp -f make.txt make2.txt
 test -f make.txt.`date +%Y%m%d` && diff make.txt make.txt.`date +%Y%m%d` && exit 1
-test -f make.txt.`date +%Y%m%d` && diff make.txt make.txt.`date +%Y%m%d` |grep '<' |sed -e 's/< //g' > make2.txt
 
 # make file
-test -d ./FILE || mkdir ./FILE
+test -d ./FILE/${FILE_DATE} || mkdir -p ./FILE/${FILE_DATE}
 
 # get client
 python3 spreadsheet_client.py |sed -e 's/],/\n/g' -e 's/]//g' -e 's/\[//g' -e "s/'//g" -e "s/ //g" > client
@@ -36,48 +40,50 @@ do
 echo $line  |grep -q Title && ID=`grep "^\`echo $line |awk -F'　' '{print $1}' |sed -e "s/Title://"\`" member |awk -F',' '{print $2}'`
 echo $line  |grep -q Title && MEM=`grep "^\`echo $line |awk -F'　' '{print $1}' |sed -e "s/Title://"\`" member |awk -F',' '{print $1}'`
 echo $line  |grep -q Title && FILE_NAME=`echo $line |sed -e "s/Title://" -e "s/　/_/g"`
-echo $line  |grep -q Title && grep "`echo $line |awk -F'　' '{print $2}'`" client >> ./FILE/${ID}_${FILE_NAME}
-echo $line  |grep -q Title || echo $line >> ./FILE/${ID}_${FILE_NAME}
+echo $line  |grep -q Title && grep "`echo $line |awk -F'　' '{print $2}'`" client >> ./FILE/${FILE_DATE}/${ID}_${FILE_NAME}
+echo $line  |grep -q Title || echo $line >> ./FILE/${FILE_DATE}/${ID}_${FILE_NAME}
 
-done < make2.txt
+done < make.txt
 
 
-mkdir DSC
+test -d ./DSC/${FILE_DATE} || mkdir -p ./DSC/${FILE_DATE}
 
 # post telegram
-for i in `ls -tr ./FILE |grep -v "キャンセル"`
+for i in `ls -tr ./FILE/${FILE_DATE} |grep -v "キャンセル"`
 do
+
+diff ./FILE/${FILE_DATE}/$i ./FILE/${OLD_FILE_DATE}/$i && continue
 
 ID=`echo $i |awk -F'_' '{print $1}'`
 MEM=`echo $i |awk -F'_' '{print $2}'`
 
-DATE=`tail -n1 ./FILE/$i`
-CLI=`head -n1 ./FILE/$i|awk -F',' '{print $2" 様  "$3"  "$4"\\\\n"$5}'`
+DATE=`tail -n1 ./FILE/${FILE_DATE}/$i`
+CLI=`head -n1 ./FILE/${FILE_DATE}/$i|awk -F',' '{print $2" 様  "$3"  "$4"\\\\n"$5}'`
 
-TXT=`cat ./FILE/$i | sed -e '1d' -e '$d' -e 's/$/ \\\\n/'`
+TXT=`cat ./FILE/${FILE_DATE}/$i | sed -e '1d' -e '$d' -e 's/$/ \\\\n/'`
 
 echo "
 $DATE \\n
 $TXT \\n
 $CLI \\n
 \\n
-\\n" >> ./DSC/${ID}_${MEM}
+\\n" >> ./DSC/${FILE_DATE}/${ID}_${MEM}
 
 done
 
-for i in `ls ./DSC`
+for i in `ls ./DSC/${FILE_DATE}`
 do
 
 ID=`echo $i |awk -F'_' '{print $1}'`
 MEM=`echo $i |awk -F'_' '{print $2}'`
 
-grep "not found" ./DSC/$i && sed -i -e "s/not found/場所確認中\\\\n※場所が決まりましたらご連絡致します/" ./DSC/$i
+grep "not found" ./DSC/${FILE_DATE}/$i && sed -i -e "s/not found/場所確認中\\\\n※場所が決まりましたらご連絡致します/" ./DSC/${FILE_DATE}/$i
 
-TXT=`cat ./DSC/$i`
+TXT=`cat ./DSC/${FILE_DATE}/$i`
 
 DSC="
 $MEM さん \\n
-前日確認です。 \\n
+当日確認です。 \\n
 \\n
 $TXT
 ご確認よろしくお願い致します。
@@ -93,8 +99,9 @@ echo ""
 
 done
 
-rm -f ./FILE/*
-rm -f ./DSC/*
+
+rm -rf ./FILE/`date +%Y%m%d -d'30 day ago'`*
+rm -rf ./DSC/`date +%Y%m%d -d'30 day ago'`*
 
 cp -f make.txt make.txt.`date +%Y%m%d`
 
